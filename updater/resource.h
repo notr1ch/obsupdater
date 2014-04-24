6aee203a713554f72487ea51d90923a92b6825b3
Richard Stanway <r.stanway@gmail.com>
2014-04-24 19:54:27 +0200

    Add an icon!

--- updater/resource.h
+++ updater/resource.h
@@ -3,6 +3,7 @@
 // Used by updater.rc
 //
 #define IDD_UPDATEDIALOG                101
+#define IDI_ICON1                       103
 #define IDC_PROGRESS                    1001
 #define IDC_STATUS                      1002
 #define IDCBUTTON                       1004
@@ -12,7 +13,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1005
 #define _APS_NEXT_SYMED_VALUE           101
